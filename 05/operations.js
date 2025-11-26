004d02b84a11566da89e72bb39ffd9e7ca6a343e
Mohit Sinha <sinhamohit663@gmail.com>
2025-11-26 05:20:06 +0000

    operations done

---
+++ 05/operations.js
@@ -0,0 +1,29 @@
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+let str1 = "Hello "
+let str2 = "Mohit"
+let str3 = str1 + str2
+
+console.table([2+2, 2-2, 2*2, 2**2, 2/2, 2%2, str3]);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log(+true);
+console.log(+"");
+
+let gameCounter = 100
+console.log(++gameCounter);
+let counter = 100
+console.log(counter++);
+
+
+
+
+
+
+

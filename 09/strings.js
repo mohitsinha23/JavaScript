6bfd05f7551f1d7645c0cf34c7169e63ee1b3281
Mohit Sinha <sinhamohit663@gmail.com>
2025-11-27 06:20:10 +0000

    strings partial done

---
+++ 09/strings.js
@@ -0,0 +1,14 @@
+const name = "mohit"
+
+const repoCount = 9
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}.`);
+
+const gameName = new String('Jerry')
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
